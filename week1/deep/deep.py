59c1a2fc330da3d4d8bc5d519c3e85d6eed27959
Tom Haugen <tomhaugen@gmail.com>
2026-06-27 18:11:05 -0500

    Updated deep.py to accomodate whitespace errors in user input.

--- week1/deep/deep.py
+++ week1/deep/deep.py
@@ -2,10 +2,15 @@ def main():
     
     # Ask user to input answer to question of the universe from Hitchhiker's Guide
     ans = input("What is the Answer to the Great Question of Life, the Universe, and Everything?")
+    
+    # Take user answer, strip whitespace and make lower case to streamline test cases in question()  
+    question(ans.lower().strip())
 
+def question(guess):
+    
     # Test for acceptable answers as outlined in problem documentation 
     # Print yes on success, print no on fail
-    if ans == "42" or ans.lower() == "forty two" or ans.lower() == "forty-two":
+    if guess == "42" or guess == "forty two" or guess == "forty-two":
         print("Yes")
     else:
         print("No")
