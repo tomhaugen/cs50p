b7ea44a9ad2b4ebfbcc9c105b13464eb0925397b
Tom Haugen <tomhaugen@gmail.com>
2026-06-27 13:35:17 -0500

    Completed einstein.py for week0 problem set.

---
+++ week0/einstein/einstein.py
@@ -0,0 +1,5 @@
+# Ask user for input of a mass in kilograms
+m = int(input("m:"))
+
+# Print corresponding energy of user's inputted mass in joules using E = mc^2 formula
+print(f"E: {m * 300000000**2}")
